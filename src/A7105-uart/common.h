5be6b8108ab3deaf3f32fdb2661c2968b997b901
Jonas Deitmerg <jonasdeitmerg@aim.com>
2015-11-18 10:34:57 +0100

    Add bit operation macros

---
+++ src/A7105-uart/common.h
@@ -0,0 +1,30 @@
+/*
+ *  This file is part of A7105-uart, a UART interface to the A7105 wireless
+ *  tranceiver.
+ *  Copyright (C) 2015 J.Deitmerg <github.com/mowfask>
+ *
+ *  A7105-uart is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  A7105-uart is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with A7105-uart.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef COMMON_H
+#define COMMON_H
+
+//Bit operations
+#define setbit(byte, bit) ((byte) |= ((1) << (bit)))
+#define clearbit(byte, bit) ((byte) &= ~((1) << (bit)))
+#define togglebit(byte, bit) ((byte) ^= ((1) << (bit)))
+#define testbit(byte, bit) (((byte) >> (bit)) & (1))    //returns 1 or 0
+
+#endif
+
